eae3b1b8a9f15e61ebedb77c98253986ae51d72f
druggedhippo <dh@gar.id.au>
2021-12-11 01:08:59 +1000

    Add files via upload

---
+++ NVIDIAShaderBrowser/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace NVIDIAShaderBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.UID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.BinSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.autoRefreshCheck = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.clearCacheButton = new System.Windows.Forms.Button();
+            this.totalTextBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.AllowColumnReorder = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.UID,
+            this.CName,
+            this.BinSize});
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(28, 29);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(760, 527);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // UID
+            // 
+            this.UID.Text = "UID";
+            this.UID.Width = 147;
+            // 
+            // CName
+            // 
+            this.CName.Text = "Process";
+            this.CName.Width = 189;
+            // 
+            // BinSize
+            // 
+            this.BinSize.Text = "Size";
+            this.BinSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.BinSize.Width = 104;
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(12, 568);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(75, 23);
+            this.refreshButton.TabIndex = 1;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // autoRefreshCheck
+            // 
+            this.autoRefreshCheck.AutoSize = true;
+            this.autoRefreshCheck.Checked = true;
+            this.autoRefreshCheck.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.autoRefreshCheck.Location = new System.Drawing.Point(141, 568);
+            this.autoRefreshCheck.Name = "autoRefreshCheck";
+            this.autoRefreshCheck.Size = new System.Drawing.Size(88, 17);
+            this.autoRefreshCheck.TabIndex = 2;
+            this.autoRefreshCheck.Text = "Auto Refresh";
+            this.autoRefreshCheck.UseVisualStyleBackColor = true;
+            this.autoRefreshCheck.CheckedChanged += new System.EventHandler(this.autoRefreshCheck_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // clearCacheButton
+            // 
+            this.clearCacheButton.Location = new System.Drawing.Point(697, 562);
+            this.clearCacheButton.Name = "clearCacheButton";
+            this.clearCacheButton.Size = new System.Drawing.Size(75, 23);
+            this.clearCacheButton.TabIndex = 3;
+            this.clearCacheButton.Text = "Clear Cache";
+            this.clearCacheButton.UseVisualStyleBackColor = true;
+            this.clearCacheButton.Click += new System.EventHandler(this.clearCacheButton_Click);
+            // 
+            // totalTextBox1
+            // 
+            this.totalTextBox1.Location = new System.Drawing.Point(420, 562);
+            this.totalTextBox1.Name = "totalTextBox1";
+            this.totalTextBox1.ReadOnly = true;
+            this.totalTextBox1.Size = new System.Drawing.Size(136, 20);
+            this.totalTextBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(365, 562);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Total";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 603);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.totalTextBox1);
+            this.Controls.Add(this.clearCacheButton);
+            this.Controls.Add(this.autoRefreshCheck);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.listView1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "NVIDIA Cache size";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader CName;
+        private System.Windows.Forms.ColumnHeader BinSize;
+        private System.Windows.Forms.ColumnHeader UID;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.CheckBox autoRefreshCheck;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button clearCacheButton;
+        private System.Windows.Forms.TextBox totalTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NVIDIAShaderBrowser/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NVIDIAShaderBrowser
+{
+    public partial class Form1 : Form
+    {
+
+        String shaderDir = "%localappdata%\\NVIDIA\\DXCache";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            doStuff();
+        }
+
+        void doStuff()
+        { 
+            Dictionary<string, List<string>> files = new Dictionary<string, List<string>>();
+
+            string[] binAndTocFiles = Directory.GetFiles(Environment.ExpandEnvironmentVariables(shaderDir), "*.*");
+            foreach (string file in binAndTocFiles)
+            {
+                string uid = getUID(file);
+                if (uid != null)
+                {
+                    if (!files.ContainsKey(uid))
+                        files.Add(uid, new List<string>());
+                    files[uid].Add(file);
+                }
+            }
+
+            this.totalTextBox1.Text = FormatBytes(binAndTocFiles.ToList().Sum(x => new FileInfo(x).Length), true);
+
+            // SortedDictionary<string, ListViewItem> items = new SortedDictionary<string, ListViewItem>();
+            //listView1.BeginUpdate();
+            foreach (KeyValuePair<string, List<string>> entry in files)
+            {
+                string processName = getProcessName(entry.Value);
+                if (processName != null)
+                {
+
+
+                    ListViewItem item;
+                    if (listView1.Items.ContainsKey(entry.Key))
+                    {
+                        item = listView1.Items[entry.Key];
+                        //item.SubItems[1].Text = processName;
+                        string newSum = (FormatBytes(entry.Value.Sum(s => new FileInfo(s).Length), true));
+                        if (!item.SubItems[2].Text.Equals(newSum))
+                            item.SubItems[2].Text = newSum;
+                    }
+                    else
+                    {
+                        item = new ListViewItem();
+                        item.Text = entry.Key;
+                        item.Name = entry.Key;
+                        item.SubItems.Add(processName);
+                        item.SubItems.Add(FormatBytes(entry.Value.Sum(s => new FileInfo(s).Length), true));
+                        listView1.Items.Add(item);
+                    }
+                }
+            }
+
+            List<ListViewItem> itemsToRemove = new List<ListViewItem>();
+            foreach(ListViewItem i in listView1.Items)
+            {
+                if (!files.ContainsKey(i.Text))
+                    itemsToRemove.Add(i);
+            }
+            itemsToRemove.ToList().ForEach(x => listView1.Items.Remove(x));
+            //listView1.EndUpdate();
+
+        }
+
+        /** Stolen from stackoverflow https://stackoverflow.com/questions/1242266/converting-bytes-to-gb-in-c */
+        //note: this is the JLopez answer!!
+        /// <summary>
+        /// Return size in human readable form
+        /// </summary>
+        /// <param name="bytes">Size in bytes</param>
+        /// <param name="useUnit ">Includes measure unit (default: false)</param>
+        /// <returns>Readable value</returns>
+        public static string FormatBytes(long bytes, bool useUnit = false)
+        {
+            string[] Suffix = { " B", " kB", " MB", " GB", " TB" };
+            double dblSByte = bytes;
+            int i;
+            for (i = 0; i < Suffix.Length && bytes >= 1024; i++, bytes /= 1024)
+            {
+                dblSByte = bytes / 1024.0;
+            }
+            return $"{dblSByte:0.##}{(useUnit ? Suffix[i] : null)}";
+        }
+
+        string getUID(string path)
+        {
+            string file = Path.GetFileName(path);
+            string[] parts = file.Split('_');
+            if (parts.Length > 3)
+            {
+                return parts[0] + "_" + parts[1] + "_" + parts[2];
+            }
+            return null;
+        }
+
+        string getProcessName(List<string> files)
+        {
+            try
+            {
+                foreach (string s in files)
+                {
+                    string p = getProcessName(s);
+                    if (p != null)
+                        return p;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+
+
+        string getProcessName(string file)
+        {
+            FileStream tocFs = File.OpenRead(file);
+            if (tocFs.ReadByte() == 0x44 && tocFs.ReadByte() == 0x58 && tocFs.ReadByte() == 0x44 && tocFs.ReadByte() == 0x43)
+            {
+
+                tocFs.Seek(80, SeekOrigin.Begin);
+                byte[] processNameB = new byte[255];
+                tocFs.Read(processNameB, 0, processNameB.Length);
+                return System.Text.Encoding.UTF8.GetString(processNameB, 0, processNameB.Length);
+            }
+            return null;
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            doStuff();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            doStuff();
+        }
+
+        private void autoRefreshCheck_CheckedChanged(object sender, EventArgs e)
+        {
+            if (autoRefreshCheck.Checked)
+            {
+                timer1.Enabled = true;
+            }
+            else
+                timer1.Enabled = false;
+        }
+
+        private void clearCacheButton_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Are you really sure?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (res == DialogResult.Yes)
+            {
+                purgeDir();
+                //MessageBox.Show("Complete");
+            }
+        }
+        void purgeDir()
+        {
+            Directory.GetFiles(Environment.ExpandEnvironmentVariables(shaderDir), "*.toc").ToList().ForEach(s =>
+            {
+                try
+                {
+                    File.Delete(s);
+                }
+                catch (Exception ex)
+                {
+
+                }
+            });
+            Directory.GetFiles(Environment.ExpandEnvironmentVariables(shaderDir), "*.bin").ToList().ForEach(s =>
+            {
+                try
+                {
+                    File.Delete(s);
+                }
+                catch (Exception ex)
+                {
+
+                }
+            });
+
+        }
+    }
+}
