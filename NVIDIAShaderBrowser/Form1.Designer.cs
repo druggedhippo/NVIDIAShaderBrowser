cb5b37eef1b683d0b079bc5e91a5db36a6a7737e
druggedhippo <dh@gar.id.au>
2021-12-11 21:56:32 +1000

    Add files via upload

--- NVIDIAShaderBrowser/Form1.Designer.cs
+++ NVIDIAShaderBrowser/Form1.Designer.cs
@@ -29,49 +29,21 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.UID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.CName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.BinSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.refreshButton = new System.Windows.Forms.Button();
             this.autoRefreshCheck = new System.Windows.Forms.CheckBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.clearCacheButton = new System.Windows.Forms.Button();
             this.totalTextBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.advancedListView1 = new Fluent.Lists.AdvancedListView();
+            this.uidColumn = ((Fluent.OLVColumn)(new Fluent.OLVColumn()));
+            this.processNameColumn = ((Fluent.OLVColumn)(new Fluent.OLVColumn()));
+            this.sizeColumn = ((Fluent.OLVColumn)(new Fluent.OLVColumn()));
+            this.deltaColumn = ((Fluent.OLVColumn)(new Fluent.OLVColumn()));
+            this.totalDelta = ((Fluent.OLVColumn)(new Fluent.OLVColumn()));
+            this.buttonResetTotals = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.advancedListView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // listView1
-            // 
-            this.listView1.AllowColumnReorder = true;
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.UID,
-            this.CName,
-            this.BinSize});
-            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(28, 29);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(760, 527);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // UID
-            // 
-            this.UID.Text = "UID";
-            this.UID.Width = 147;
-            // 
-            // CName
-            // 
-            this.CName.Text = "Process";
-            this.CName.Width = 189;
-            // 
-            // BinSize
-            // 
-            this.BinSize.Text = "Size";
-            this.BinSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.BinSize.Width = 104;
             // 
             // refreshButton
             // 
@@ -114,7 +86,7 @@
             // 
             // totalTextBox1
             // 
-            this.totalTextBox1.Location = new System.Drawing.Point(420, 562);
+            this.totalTextBox1.Location = new System.Drawing.Point(349, 562);
             this.totalTextBox1.Name = "totalTextBox1";
             this.totalTextBox1.ReadOnly = true;
             this.totalTextBox1.Size = new System.Drawing.Size(136, 20);
@@ -123,44 +95,119 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(365, 562);
+            this.label1.Location = new System.Drawing.Point(302, 562);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(31, 13);
             this.label1.TabIndex = 5;
             this.label1.Text = "Total";
+            // 
+            // advancedListView1
+            // 
+            this.advancedListView1.AllColumns.Add(this.uidColumn);
+            this.advancedListView1.AllColumns.Add(this.processNameColumn);
+            this.advancedListView1.AllColumns.Add(this.sizeColumn);
+            this.advancedListView1.AllColumns.Add(this.deltaColumn);
+            this.advancedListView1.AllColumns.Add(this.totalDelta);
+            this.advancedListView1.CellEditUseWholeCell = false;
+            this.advancedListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.uidColumn,
+            this.processNameColumn,
+            this.sizeColumn,
+            this.deltaColumn,
+            this.totalDelta});
+            this.advancedListView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.advancedListView1.FullRowSelect = true;
+            this.advancedListView1.GridLines = true;
+            this.advancedListView1.HasCollapsibleGroups = false;
+            this.advancedListView1.HideSelection = false;
+            this.advancedListView1.Location = new System.Drawing.Point(12, 13);
+            this.advancedListView1.Name = "advancedListView1";
+            this.advancedListView1.ShowGroups = false;
+            this.advancedListView1.Size = new System.Drawing.Size(760, 532);
+            this.advancedListView1.TabIndex = 6;
+            this.advancedListView1.UseCompatibleStateImageBehavior = false;
+            this.advancedListView1.UseNotifyPropertyChanged = true;
+            this.advancedListView1.View = System.Windows.Forms.View.Details;
+            // 
+            // uidColumn
+            // 
+            this.uidColumn.AspectName = "Uid";
+            this.uidColumn.IsEditable = false;
+            this.uidColumn.Text = "UID";
+            this.uidColumn.Width = 155;
+            // 
+            // processNameColumn
+            // 
+            this.processNameColumn.AspectName = "ProcessName";
+            this.processNameColumn.IsEditable = false;
+            this.processNameColumn.Text = "Process";
+            this.processNameColumn.Width = 167;
+            // 
+            // sizeColumn
+            // 
+            this.sizeColumn.AspectName = "Size";
+            this.sizeColumn.IsEditable = false;
+            this.sizeColumn.Text = "Size";
+            this.sizeColumn.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // deltaColumn
+            // 
+            this.deltaColumn.AspectName = "Delta";
+            this.deltaColumn.Text = "Delta Change";
+            this.deltaColumn.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.deltaColumn.Width = 96;
+            // 
+            // totalDelta
+            // 
+            this.totalDelta.AspectName = "TotalDelta";
+            this.totalDelta.Text = "Cummulative Delta";
+            // 
+            // buttonResetTotals
+            // 
+            this.buttonResetTotals.Location = new System.Drawing.Point(573, 562);
+            this.buttonResetTotals.Name = "buttonResetTotals";
+            this.buttonResetTotals.Size = new System.Drawing.Size(75, 23);
+            this.buttonResetTotals.TabIndex = 7;
+            this.buttonResetTotals.Text = "Reset totals";
+            this.buttonResetTotals.UseVisualStyleBackColor = true;
+            this.buttonResetTotals.Click += new System.EventHandler(this.buttonResetTotals_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 603);
+            this.Controls.Add(this.buttonResetTotals);
+            this.Controls.Add(this.advancedListView1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.totalTextBox1);
             this.Controls.Add(this.clearCacheButton);
             this.Controls.Add(this.autoRefreshCheck);
             this.Controls.Add(this.refreshButton);
-            this.Controls.Add(this.listView1);
             this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "NVIDIA Cache size";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.advancedListView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ColumnHeader CName;
-        private System.Windows.Forms.ColumnHeader BinSize;
-        private System.Windows.Forms.ColumnHeader UID;
         private System.Windows.Forms.Button refreshButton;
         private System.Windows.Forms.CheckBox autoRefreshCheck;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button clearCacheButton;
         private System.Windows.Forms.TextBox totalTextBox1;
         private System.Windows.Forms.Label label1;
+        private Fluent.Lists.AdvancedListView advancedListView1;
+        private Fluent.OLVColumn uidColumn;
+        private Fluent.OLVColumn processNameColumn;
+        private Fluent.OLVColumn sizeColumn;
+        private Fluent.OLVColumn deltaColumn;
+        private Fluent.OLVColumn totalDelta;
+        private System.Windows.Forms.Button buttonResetTotals;
     }
 }
 
